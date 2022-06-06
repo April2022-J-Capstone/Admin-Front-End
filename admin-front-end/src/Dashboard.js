215820a32e270cb9cff9d83ecb5a3a319e869d6a
MarcusScottS <marcus.sauceda@smoothstack.com>
2022-06-06 11:45:11 -0500

    removed react-admin folder

--- admin-front-end/src/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { Card, CardContent, CardHeader } from '@mui/material';
-
-export default () => (
-    <Card>
-        <CardHeader title="Welcome to the administration" />
-        <CardContent>Lirem ipsum dolor amet...</CardContent>
-    </Card>
-)
