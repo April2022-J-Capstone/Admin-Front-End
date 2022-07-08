cc25bbfb77f0828aa4a6f9338bdbbe507931c081
Sinblio-SmoothStack <benls@iastate.edu>
2022-07-08 08:21:31 -0500

    Added a data page for viewing an individual item

---
+++ admin-front-end-react/src/pages/data-page/DataPage.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import Table from "../../components/tables/Table";
+
+const DataPage = (props) => {
+    return (
+        <Table
+
+        />
+    );
+}
+
+export default DataPage;
